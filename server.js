283941408cdf3fdc56bb6082112ee209cf1f5eac
Inge van Zetten <maizette@avans.nl>
2018-04-26 14:37:45 +0200

    In progress

--- server.js
+++ server.js
@@ -38,41 +38,7 @@ app.use('*', (req, res, next) => {
   next()
 })
 
-//api routes
-//lijst met personen
-app.get('/api/all', (req, res, next) => {
-  console.log('get was called')
-  res.status(200).json(personlist).end()
-})
 
-//1 persoon op id teruggeven
-app.get('/api/person/:id', (req, res, next) => {
-  console.log('get was called')
-  const id = req.params.id
-
-  if(id >=0 && id < personlist.length){
-//als id geldig is in de personlist: stuur persoon terug
-  res.status(200).json(personlist[id]).end()
-  }else{
-    //als id niet geldig is: error
-    const error = {
-      error: 'Id does not exist (index out of bounds)'
-    }
-    next(error)
-  }
-})
-
-app.post('/api/person', (req, res, next) => {
-  console.log('post was called')
-  console.log(req.body)
-
-  const firstname = req.body.firstname
-  const lastname = req.body.lastname
-  const person = new Person(firstname, lastname)
-  personlist.push(person)
-
-  res.status(200).json(person).end()
-})
 
 //wanneer de gevraagde endpoint niet gevonden is komen we hier.
 //functies get en post zijn niet aangeroepen
